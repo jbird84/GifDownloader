c89c0a1ce53626686f44bd846da05e0f764865db
DoodMac <48062579+jbird84@users.noreply.github.com>
2022-12-20 17:00:22 -0600

    Initial Commit

---
+++ GifDownloader/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  GifDownloader
+//
+//  Created by Kinney Kare on 12/20/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
